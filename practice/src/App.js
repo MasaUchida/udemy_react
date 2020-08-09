c82a098422368aac6e474e6bcfa1056a4121f9a8
Masatoshi Uchida <uchidamasa.design@gmail.com>
2020-08-09 17:13:47 +0900

    APPを改造

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 function App() {
+  const dom = <h1 className="hoge">yes</h1>
+  const form = <input type="text" onChange={(e) => {console.log(e.target.value);}}></input>
+
   return (
-    <div className="App">
-    <h1>Hellow world!</h1>
+    <div>
+      {form}
+      {dom}
+      <h1>Hellow world!</h1>
     </div>
   );
 }
